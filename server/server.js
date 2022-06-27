dbc68b3370d432407b7d39fd71339d8b9be5f717
moonlight137 <luis_4664@hotmail.com>
2022-06-27 11:35:57 -0700

    'Message'

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path'); 
+const http = require('http');
 const express = require('express'); 
+const socketIO = require('socket.io'); 
 
 
 
@@ -7,10 +9,31 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000; 
 
 var app = express(); 
+var server = http.createServer(app);
+var io = socketIO(server); 
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected'); 
+
+    socket.emit('newMessage', {
+       from: 'Michael', 
+       text: 'see u then c:',
+       createdAt: 123123
+    }); 
+
+    socket.on('createMessage', (message) => {
+        console.log('creatMessage', message); 
+    }); 
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected '); 
+    }); 
+});
+
+
+server.listen(3000, () => {
     console.log(`Server is up on ${port}`); 
 }); 
 
