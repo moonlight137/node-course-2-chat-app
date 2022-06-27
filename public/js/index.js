70e97434a76bdf52525b58946485438684a52dac
moonlight137 <luis_4664@hotmail.com>
2022-06-27 15:56:56 -0700

    'AddCss'

--- public/js/index.js
+++ public/js/index.js
@@ -32,11 +32,14 @@ socket.on('newLocationMessage' , function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault(); 
+
+    var messageTextbox = jQuery('[name=message]'); 
+
     socket.emit('createMessage' , {
         from: 'User', 
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -46,12 +49,17 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported bu your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('send location'); 
         socket.emit('createLocationMessage', {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('send location');
         alert('unable to fetch location');
+
     });
 });
