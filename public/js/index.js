a61e24c8eb1cd851d7bf4339ccad5cc447879c1f
moonlight137 <luis_4664@hotmail.com>
2022-06-27 12:58:23 -0700

    'AddForMessage'

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server '); 
-
-    socket.emit('createMessage', {
-        from: 'Luis',
-        text: 'yup, thats works for me'
-    });
 }); 
 
 socket.on('disconnect', function () {
@@ -15,4 +10,21 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('NewMessage', message); 
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`); 
+
+    jQuery('#messages').append(li); 
+
+});
+
+
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault(); 
+    socket.emit('createMessage' , {
+        from: 'User', 
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
 });
