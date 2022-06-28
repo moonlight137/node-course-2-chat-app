fc78c215490121618bd046e454170cfebae134ab
moonlight137 <luis_4664@hotmail.com>
2022-06-28 11:33:56 -0700

    'sendmessages'

--- public/js/chat.js
+++ public/js/chat.js
@@ -82,8 +82,7 @@ jQuery('#message-form').on('submit', function (e) {
 
     var messageTextbox = jQuery('[name=message]'); 
 
-    socket.emit('createMessage' , {
-        from: 'User', 
+    socket.emit('createMessage' , { 
         text: messageTextbox.val()
     }, function () {
         messageTextbox.val('');
